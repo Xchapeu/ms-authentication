0bc75bc7d0d15e6494081127b3c167769287086f
Xchapeu <xchapeu@gmail.com>
2022-02-26 11:14:17 -0300

    Add bearer authentication

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://hndrmftk:p_fRGut0PgjewipJK0HQJ78H5qd7IAeC@kesavan.db.elephantsql.com/hndrmftk';
+const connectionString = "postgres://hndrmftk:p_fRGut0PgjewipJK0HQJ78H5qd7IAeC@kesavan.db.elephantsql.com/hndrmftk";
 const db = new Pool({ connectionString });
 
 export default db;
---
+++ src/middlewares/bearer-authentication.middleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import ForbiddenError from "../models/errors/forbidden.error.model";
+import JWT from 'jsonwebtoken';
+
+async function bearerAuthenticationMiddleware(req: Request, res: Response, next: NextFunction) {
+  try {
+    const authorizationHeader = req.headers['authorization'];
+
+    if(!authorizationHeader) {
+      throw new ForbiddenError("Credenciais não informadas");
+    }
+
+    const [authenticationType, token] = authorizationHeader.split(' ');
+
+    if(authenticationType !== 'Bearer' || !token) {
+      throw new ForbiddenError("Tipo de autenticação inválida.");
+    }
+
+    const tokenPayload = JWT.verify(token, 'my_secret_key');
+
+    if(typeof tokenPayload !== 'object' || !tokenPayload.sub) {
+      throw new ForbiddenError("Token inválido");
+    }
+
+    const user = { 
+      uuid: tokenPayload.sub, 
+      username: tokenPayload.username 
+    };
+
+    req.user = user;
+
+    next();    
+  } catch (err) {
+    next(err)
+  }
+}
+
+export default bearerAuthenticationMiddleware;
+
+//{
+// 	"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjQ1NjYxMjg0LCJzdWIiOiIxOWUwNThlYy1hNTFmLTQ0M2QtODg2Yi04ZWZlMTVkNjBmNzAifQ.N5LwmkAevWPkJlufXSdwGnDlJLKKlaiCbPe_g74lUK4"
+// }
